be460b3bd1e6e6f084324554340b3f1f0f744ea4
PaolaP <paolaperez8@gmail.com>
2022-02-20 22:28:22 -0500

    update state of components

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 
 function CounterButton(props) {
   const [clickAmount, setClickAmount] = useState(0);
-  return (
-    <button className="CounterButton">
-    You clicked me {clickAmount} times
-  </button>
-  );
+
+  const handleClick = () => setClickAmount(clickAmount + 1);
+
+   return (
+      <button onClick={handleClick} className="CounterButton">
+        You clicked me {clickAmount} times
+      </button>
+    );
 }
 
 export default CounterButton;

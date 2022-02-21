90f695470f928ae3cf545764cc5b2c25fa1a7e87
PaolaP <paolaperez8@gmail.com>
2022-02-21 14:10:00 +0100

    lift state up and pass down values turn on off

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   const [light, setLight] = useState('off');
   const dark = (light === 'off') ? 'dark' : '';
+  const switchLight = () => setLight((light === "on") ? "off" : "on");
 
   return (
     <div className={`App ${dark}`}> 
       <h1>Fancy Buttons!</h1>
-      <section>
+      
         <AngryButton />
         <CounterButton />
         <TextRepeaterButton />
         <LightSwitchButton light={light} setLight={setLight}/>
-      </section>
-    </div>
-  );
-}
+      </div>
+    );
+  }
+
 
 export default App;
--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
 function LightSwitchButton(props) {
-  const [light, setLight] = useState("off");
-
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  console.log("=========", props);
+  const { light, setLight } = props;
+  const handleClick = () => {
+    if (light === "on") {
+      setLight("off")
+    } else {
+      setLight("on");
+    }
+  };
 
   return (
-      <button onClick={handleClick} className="LightSwitchButton">
+
+      <button onClick={handleClick } className="LightSwitchButton">
         {light === 'on' && <span><i>💡</i> I'm on!</span>}
         {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
       </button>

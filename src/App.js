e63e51e53a76b55bcc7bdf5ffc21685b23534c80
PaolaP <paolaperez8@gmail.com>
2022-02-20 23:15:43 -0500

    lift stateApp for dark mode app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react";
 import './App.css';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
@@ -6,14 +6,17 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+
   return (
-    <div className={`App`}>
+    <div className={`App ${dark}`}> 
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
         <TextRepeaterButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
       </section>
     </div>
   );
